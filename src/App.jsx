9c990323430ff76796f153904b1ca6911dcbb06d
shokhsanam18 <shokhsanam18@gmail.com>
2025-03-10 22:15:19 +0500

    feat: added material-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@material-tailwind/react";
+
 function App() {
   return (
     <>
-      <h1>hello dashboard</h1>
+      <h1 className="bg-red-400">hello dashboard</h1>
+      <Button>hello world</Button>
     </>
   );
 }

a45b6eff7c410599e2bff6a390b1df237e988890
Charos Isroilova <charosbakhtiyarovna001@gmail.com>
2025-03-13 17:56:22 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-red-400">hello dashboard</h1>
+      <Button>hello world</Button>
+    </>
+  );
+}
+
+export default App;

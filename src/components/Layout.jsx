7c0193daa55fa668398f6c6b8fae8c7088099c01
Inobat <inobatmirahmedova@gmail.com>
2025-03-14 02:35:11 +0500

    feat: SidebarWithBurgerMenu added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { SidebarWithBurgerMenu } from "./Sidebar";
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <>
+      <div
+        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+      >
+        <SidebarWithBurgerMenu/>
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 export default Layout

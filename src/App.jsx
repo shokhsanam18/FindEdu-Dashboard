e9702980ddc45a4c8c1a66c33178217236bee6a2
Charos Isroilova <charosbakhtiyarovna001@gmail.com>
2025-03-16 19:06:17 +0500

    Table with users data inside in the user page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Button } from "@material-tailwind/react";
+import UserPage from "./User-page/User-page";
 
 function App() {
   return (
-    <>
-      <h1 className="bg-red-400">hello dashboard</h1>
-      <Button>hello world</Button>
-    </>
+    <Router>
+      <div className="p-4">
+        <h1 className="bg-red-400 p-2">hello dashboard</h1>
+        <Button>Hello world</Button>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 className="text-center mt-4">Home Page</h1>}
+        />
+        <Route path="/users" element={<UserPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/User-page/User-page.jsx
@@ -0,0 +1,12 @@
+import Table from "../Components-user/Table";
+
+function UserPage() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4">Users</h2>
+      <Table />
+    </div>
+  );
+}
+
+export default UserPage;

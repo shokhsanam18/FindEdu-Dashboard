d48e7945d9040272a48d88a2b92c8e566474b94a
navydandelion <kuvondikovaruhshona@gmail.com>
2025-03-18 10:12:59 +0500

    added main-page

--- src/pages/mainpage/Main.jsx
+++ src/pages/mainpage/Main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./header";
 import CategoriesMajors from "./Majors";
 import VisitorChart from "./linegraph.jsx";
 const Main = () => {
   return (
     <div className="flex-1">
-      <Header />
       <CategoriesMajors />
       <VisitorChart />
     </div>
